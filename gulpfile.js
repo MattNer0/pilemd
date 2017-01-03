820f4b8131fbd3b20fa7cb41fd9fc2c82637ef14
MattNer0 <matteo.balloco@gmail.com>
2017-01-03 11:51:43 +0100

    version number

--- gulpfile.js
+++ gulpfile.js
@@ -12,14 +12,11 @@ gulp.task('default', function() {
     .pipe(gulp.dest('dist/js/'));
 });
 
-
 var BASE_OPTION = {
     dir: '.',
     overwrite: true,
     arch: 'x64',
     version: '0.37.3',
-    'build-version': '0.9.0',
-    'app-version': '0.9.0',
     ignore: '(node_modules\/(codemirror|highlight.js|marked|vue|vue-resource)|src|icons|releases|.idea.*|README\.md|\.DS_Store|env|gulpfile\.js|webpack\.config\.js|\.gitignore)',
     asar: true,
     prune: true
@@ -33,7 +30,8 @@ gulp.task('electron', function(done) {
     sign: process.env['PM_OSX_SIGN'],
     'helper-buldle-id': 'md.pile.helper',
     'app-bundle-id': 'md.pile'
-  }), function() {
+  }), function(err) {
+    if(err) console.log(err);
     done();
   });
 });
@@ -43,7 +41,8 @@ gulp.task('electron-linux', function(done) {
   packager(_.defaults(_.clone(BASE_OPTION), {
     out: './releases/linux/',
     platform: 'linux'
-  }), function() {
+  }), function(err) {
+    if(err) console.log(err);
     done();
   });
 });
@@ -56,7 +55,8 @@ gulp.task('electron-windows', function(done) {
     icon: './icons/pilemd.ico'
   });
   // c['arch'] = 'ia32';
-  packager(c, function() {
+  packager(c, function(err) {
+    if(err) console.log(err);
     done();
   });
 });
