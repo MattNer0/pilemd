3ef2c2c5d541d62b000abe984cfe62afd7acd4fd
MattNer0 <matteo.balloco@gmail.com>
2017-08-15 19:59:03 +0200

    electron update

--- gulpfile.js
+++ gulpfile.js
@@ -6,57 +6,57 @@ var _ = require('lodash');
 var fs = require('fs');
 
 gulp.task('default', function() {
-  return gulp.src('src/entry.js')
-    .pipe(webpack( require('./webpack.config.js') ))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js/'));
+	return gulp.src('src/entry.js')
+		.pipe(webpack(require('./webpack.config.js')))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js/'));
 });
 
 var BASE_OPTION = {
-    dir: '.',
-    overwrite: true,
-    arch: 'x64',
-    electronVersion: '1.6.11',
-    ignore: '(node_modules\/(codemirror|highlight.js|marked|vue|vue-resource)|src|icons|releases|.idea.*|README\.md|\.DS_Store|env|gulpfile\.js|webpack\.config\.js|\.gitignore)',
-    asar: true,
-    prune: true
+	dir: '.',
+	overwrite: true,
+	arch: 'x64',
+	//electronVersion: '1.7.5',
+	ignore: '(node_modules\/(codemirror|highlight.js|marked|vue|vue-resource)|src|icons|releases|.idea.*|README\.md|\.DS_Store|env|gulpfile\.js|webpack\.config\.js|\.gitignore|\.gjslintrc)',
+	asar: true,
+	prune: false
 };
 
 gulp.task('electron', function(done) {
-  packager(_.defaults(_.clone(BASE_OPTION), {
-    out: 'releases/darwin/',
-    platform: 'darwin',
-    icon: './icons/pilemd.icns',
-    sign: process.env['PM_OSX_SIGN'],
-    'helper-buldle-id': 'md.pile.helper',
-    'app-bundle-id': 'md.pile'
-  }), function(err) {
-    if(err) console.log(err);
-    done();
-  });
+	packager(_.defaults(_.clone(BASE_OPTION), {
+		out: 'releases/darwin/',
+		platform: 'darwin',
+		icon: './icons/pilemd.icns',
+		sign: process.env['PM_OSX_SIGN'],
+		'helper-buldle-id': 'md.pile.helper',
+		'app-bundle-id': 'md.pile'
+	}), function(err) {
+		if (err) console.log(err);
+		done();
+	});
 });
 
 
 gulp.task('electron-linux', function(done) {
-  packager(_.defaults(_.clone(BASE_OPTION), {
-    out: './releases/linux/',
-    platform: 'linux'
-  }), function(err) {
-    if(err) console.log(err);
-    done();
-  });
+	packager(_.defaults(_.clone(BASE_OPTION), {
+		out: './releases/linux/',
+		platform: 'linux'
+	}), function(err) {
+		if (err) console.log(err);
+		done();
+	});
 });
 
 
 gulp.task('electron-windows', function(done) {
-  var c = _.defaults(_.clone(BASE_OPTION), {
-    out: './releases/windows/',
-    platform: 'win32',
-    icon: './icons/pilemd.ico'
-  });
-  // c['arch'] = 'ia32';
-  packager(c, function(err) {
-    if(err) console.log(err);
-    done();
-  });
+	var c = _.defaults(_.clone(BASE_OPTION), {
+		out: './releases/windows/',
+		platform: 'win32',
+		icon: './icons/pilemd.ico'
+	});
+	// c['arch'] = 'ia32';
+	packager(c, function(err) {
+		if (err) console.log(err);
+		done();
+	});
 });
