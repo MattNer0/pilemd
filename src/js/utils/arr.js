d0cb4f28af57a3353dc3e9bd0cf735ca321e7758
MattNer0 <matteo.balloco@gmail.com>
2017-09-13 10:50:47 +0200

    code doc

--- src/js/utils/arr.js
+++ src/js/utils/arr.js
@@ -1,6 +1,10 @@
 module.exports = {
 	/**
-	 * Sort array of objects by property
+	 * Sort array by a certain key
+	 * @param  {Array}    objs      Array of objects
+	 * @param  {String}   property  Name of the property inside the objects
+	 * @param  {Boolean}  asc       Order flag, True if ASC, False if DESC
+	 * @return {Array}              The Array of objects after ordering
 	 */
 	sortBy(objs, property, asc) {
 		asc = asc ? true : false;
@@ -10,6 +14,12 @@ module.exports = {
 			else { return 0 }
 		});
 	},
+	/**
+	 * Remove one element from array.
+	 * @param  {Array}   objs      Array of objects
+	 * @param  {Object}  detector  The Object
+	 * @return {Array}             The Array after the object was removed
+	 */
 	remove(objs, detector) {
 		if (!objs.length) return;
 		var t = objs.find(detector);
